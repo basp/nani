18f16ce633a4cda3ee3d5a310e1321aaaae58259
Bas Pennings <baspster@gmail.com>
2019-11-19 18:28:02 +0100

    initial commit

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "nani")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/nani.scrbl" ())))
+(define pkg-desc "Description Here")
+(define version "0.0")
+(define pkg-authors '(Bas))
---
+++ scribblings/nani.scrbl
@@ -0,0 +1,10 @@
+#lang scribble/manual
+@require[@for-label[nani
+                    racket/base]]
+
+@title{nani}
+@author{Bas}
+
+@defmodule[nani]
+
+Package Description Here
